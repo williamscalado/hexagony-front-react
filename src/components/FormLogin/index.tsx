c7d61edcdcc42e94aac2dda2ba58e97b61b2dc03
Williams Calado <williamscalado@gmail.com>
2022-06-27 18:27:57 -0300

    create validation login form

--- src/components/FormLogin/index.tsx
+++ src/components/FormLogin/index.tsx
@@ -1,13 +1,68 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FormEvent, useState } from "react";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 import "./style.scss";
+
+type formLogin = {
+	email: string;
+	password: string;
+};
+
+const formRules: yup.SchemaOf<formLogin> = yup.object().shape({
+	email: yup
+		.string()
+		.email("E-mail inválido!")
+		.required("Precisamos do seu e-mail"),
+	password: yup
+		.string()
+		.min(8, "Senha curta, mínimo 8 caracteres")
+		.required("Digite uma senha válida"),
+});
+
 export const FormLogin = () => {
+	const [formData, setFormData] = useState({});
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<formLogin>({
+		resolver: yupResolver(formRules),
+	});
+
+	const handleInputChange = (e: FormEvent<HTMLInputElement>) => {
+		const input = e.target as HTMLInputElement;
+		setFormData({
+			...formData,
+			[input.name]: input.value,
+		});
+	};
+
+	const onSubmit = async (data: formLogin) => {
+		console.log(data);
+	};
+
 	return (
 		<div className="ContainerLogin">
-			<form action="">
+			<form onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="">E-mail </label>
-				<input type="text" />
+				<input
+					{...register("email", { required: true })}
+					type="text"
+					onChange={handleInputChange}
+					placeholder="email@x.com.br"
+				/>
+				{errors.email && <span>{errors.email.message}</span>}
 				<label htmlFor="">Senha </label>
-				<input type="text" />
-				<button>Entrar</button>
+				<input
+					type="password"
+					{...register("password", { required: true })}
+					placeholder="*****"
+					onChange={handleInputChange}
+				/>
+				{errors.password && <span>{errors.password.message}</span>}
+
+				<button type="submit">Entrar</button>
 			</form>
 		</div>
 	);
