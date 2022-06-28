3a21363cf46fbc093a2bec154f6463e843a416ff
Williams Calado <williamscalado@gmail.com>
2022-06-28 16:56:47 -0300

    update list album

--- src/components/listAlbum/index.tsx
+++ src/components/listAlbum/index.tsx
@@ -1,9 +1,25 @@
+import { AiOutlineDelete } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
 import "./style.scss";
-
 export const ListAlbum = () => {
 	return (
-		<div className="container-lis-album">
-			<p>List 1</p>
+		<div className="container-list-album">
+			<h3>Albums</h3>
+			<div className="list-album">
+				<div className="list-album-info">
+					<span>Name Album</span>
+					<span>Length 8</span>
+				</div>
+
+				<div className="list-album-icons">
+					<span>
+						<FiEdit />
+					</span>
+					<span>
+						<AiOutlineDelete onClick={() => {}} />
+					</span>
+				</div>
+			</div>
 		</div>
 	);
 };
