355b5ac0dffe4922ce69dc8a49cc73eb6264597c
Williams Calado <williamscalado@gmail.com>
2022-06-28 18:27:09 -0300

    update css

--- src/components/listAlbum/index.tsx
+++ src/components/listAlbum/index.tsx
@@ -1,25 +1,51 @@
+import { useEffect, useState } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FiEdit } from "react-icons/fi";
+import Api from "../../service/Api";
 import "./style.scss";
+
+interface IAlbums {
+	created_at: string;
+	id: string;
+	length: number;
+	name: string;
+	updated_at: string;
+}
+
 export const ListAlbum = () => {
+	const [albumData, setAlbumData] = useState([{} as IAlbums]);
+
+	const getAllAlbums = async () => {
+		const result = await Api.get("/album");
+		if (!result.data) return;
+		setAlbumData(result.data);
+	};
+	useEffect(() => {
+		getAllAlbums();
+	}, []);
+	console.log(albumData);
 	return (
 		<div className="container-list-album">
 			<h3>Albums</h3>
-			<div className="list-album">
-				<div className="list-album-info">
-					<span>Name Album</span>
-					<span>Length 8</span>
-				</div>
+			{albumData.map((item) => {
+				return (
+					<div key={item.id} className="list-album">
+						<div className="list-album-info">
+							<span>{item.name}</span>
+							<span>length {item.length}</span>
+						</div>
 
-				<div className="list-album-icons">
-					<span>
-						<FiEdit />
-					</span>
-					<span>
-						<AiOutlineDelete onClick={() => {}} />
-					</span>
-				</div>
-			</div>
+						<div className="list-album-icons">
+							<span>
+								<FiEdit />
+							</span>
+							<span>
+								<AiOutlineDelete onClick={() => {}} />
+							</span>
+						</div>
+					</div>
+				);
+			})}
 		</div>
 	);
 };
--- src/components/newAlbum/intex.tsx
+++ src/components/newAlbum/intex.tsx
@@ -1,15 +1,45 @@
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
 import "./style.scss";
+
+interface INewAlbum {
+	name: string;
+	length: string;
+}
+
+const newAlbumFormRule: yup.SchemaOf<INewAlbum> = yup.object().shape({
+	name: yup.string().required().min(3),
+	length: yup.string().required().min(1),
+});
+
 export const NewAlbum = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<INewAlbum>({
+		mode: "onBlur",
+		reValidateMode: "onBlur",
+		shouldFocusError: true,
+		resolver: yupResolver(newAlbumFormRule),
+	});
+
+	const onSubmit = async (data: INewAlbum) => {
+		console.log(data);
+		return data;
+	};
 	return (
 		<div className="container-new-album">
 			<div className="content-new-album">
 				<h3>New Album</h3>
-				<form action="">
-					<span>Name</span>
-					<input type="text" />
-					<span>Length</span>
-					<input type="number" />
-
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<label htmlFor="">Name</label>
+					<input {...register("name", { required: true })} type="text" />
+					{errors?.name && <span>{errors?.name?.message} </span>}
+					<label htmlFor="">Length</label>
+					<input {...register("length", { required: true })} type="number" />
+					{errors?.length && <span>{errors.length.message}</span>}
 					<button>Add</button>
 				</form>
 			</div>
