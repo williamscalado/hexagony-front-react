fe2eee8d369a471f8cb82e6e0dc5f789b73b0827
Williams Calado <williamscalado@gmail.com>
2022-06-29 22:33:11 -0300

    update album listy

--- src/components/listAlbum/index.tsx
+++ src/components/listAlbum/index.tsx
@@ -1,10 +1,22 @@
+import "react-confirm-alert/src/react-confirm-alert.css";
+import toast from "react-hot-toast";
 import { AiOutlineDelete } from "react-icons/ai";
 import { FiEdit } from "react-icons/fi";
 import { AlbumUseCase, IAlbums } from "../../module/album/useCase";
 import "./style.scss";
 
 export const ListAlbum = () => {
-	const albumData = AlbumUseCase.GetAllAlbum();
+	const { GetAllAlbum } = AlbumUseCase;
+
+	const handleDeleteAlbum = (id: string) => {
+		if (!id) toast.error("Error processing request!");
+
+		console.log(id);
+	};
+
+	const albumData = GetAllAlbum();
+
+	console.log(albumData);
 	return (
 		<div className="container-list-album">
 			<h3>Albums</h3>
@@ -24,7 +36,7 @@ export const ListAlbum = () => {
 									<FiEdit />
 								</span>
 								<span>
-									<AiOutlineDelete onClick={() => {}} />
+									<AiOutlineDelete onClick={() => handleDeleteAlbum(item.id)} />
 								</span>
 							</div>
 						</div>
