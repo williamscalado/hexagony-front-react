ced0dbd93829137c2b70d440792d28b39d59bc32
Williams Calado <williamscalado@gmail.com>
2022-07-05 16:51:03 -0300

    update

--- src/components/newUser/index.tsx
+++ src/components/newUser/index.tsx
@@ -1,15 +1,18 @@
 import { yupResolver } from "@hookform/resolvers/yup";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import toast from "react-hot-toast";
+import { BsEyeSlash } from "react-icons/bs";
 import { useSetRecoilState } from "recoil";
 import { IUser } from "../../modules/users/domain";
 import { userUseCase } from "../../modules/users/usecase";
 import { userFormValidation } from "../../modules/users/validation";
 import { userState } from "../../state/userState";
 import "./style.scss";
-
 export const NewUserForm = () => {
 	const setUserAllUser = useSetRecoilState<IUser[]>(userState);
+	const [showPassword, setShowPassword] = useState(false);
+
 	const {
 		register,
 		handleSubmit,
@@ -48,15 +51,31 @@ export const NewUserForm = () => {
 				<label htmlFor="email">Email</label>
 				<input type="text" {...register("email")} />
 				<span>{errors?.email && errors.email.message}</span>
+
 				<label htmlFor="password">Password</label>
-				<input type="password" {...register("password")} />
+				<span className="user-password">
+					<input type="password" {...register("password")} />
+					<button>{showPassword ? <BsEyeSlash /> : <BsEyeSlash />}</button>
+				</span>
 				<span>{errors?.password && errors.password.message}</span>
 				<label htmlFor="passwordConfirmation">Confirm Password</label>
-				<input type="password" {...register("passwordConfirmation")} />
+				<span className="user-password">
+					<input type="password" {...register("passwordConfirmation")} />
+					<button
+						type="button"
+						onClick={() => {
+							console.log("ok");
+						}}
+					>
+						{showPassword ? <BsEyeSlash /> : <BsEyeSlash />}
+					</button>
+				</span>
 				<span>
 					{errors.passwordConfirmation && errors.passwordConfirmation.message}
 				</span>
-				<button>Save</button>
+				<button type="submit" className="buttonSend">
+					Save
+				</button>
 			</form>
 		</div>
 	);
