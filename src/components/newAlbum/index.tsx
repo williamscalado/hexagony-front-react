0de1eb6dd5d382043c761835705ebf033167f4d7
Cyro Dubeux <xorycx@gmail.com>
2022-07-06 17:32:38 -0300

    user validation improvement

--- src/components/newAlbum/index.tsx
+++ src/components/newAlbum/index.tsx
@@ -10,7 +10,7 @@ import "./style.scss";
 
 const newAlbumFormRule: yup.SchemaOf<IAlbums> = yup.object().shape({
 	id: yup.string().notRequired(),
-	name: yup.string().required().min(3),
+	name: yup.string().required("full name is a required field").min(3),
 	length: yup.number().typeError("length must be a number").required().min(1),
 	created_at: yup.string().notRequired(),
 	updated_at: yup.string().notRequired(),
